6a74be247c07be9c706721eac1ea3aceb5e810cc
pawel.niechoda <pawel.niechoda@relex.fi>
2017-12-23 12:30:32 +0100

    modify

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const R = require('ramda')
 const sourceFile = process.argv[2];
 const targetFile = process.argv[3];
 
-console.log(process.argv);
-
 let getWordsToFillTheLine = function (length, words) {
   let word;
   let i = 0;
@@ -16,13 +14,13 @@ let getWordsToFillTheLine = function (length, words) {
     }
   }
 
-  if (word && word.length == length) {
+  if (word && word.length === length) {
     return [word]
   } else if (word && word.length < length) {
     return [word].concat(getWordsToFillTheLine(length - word.length - 1, words.slice(i + 1)))
   }
   return [];
-}
+};
 
 const input = fs.readFileSync(sourceFile, 'utf8');
 
@@ -31,7 +29,7 @@ let words = R.compose(
   R.sortBy(w => w.length),
   R.reject(w => w === ''),
   R.split(/\s/),
-)(input)
+)(input);
 
 const results = [];
 const lineLength = 80;
@@ -44,7 +42,7 @@ while (true) {
 
     foundWords.forEach(foundWord => {
       const index = words.indexOf(foundWord);
-      if (index != -1) {
+      if (index !== -1) {
         words.splice(index, 1);
       }
     });
